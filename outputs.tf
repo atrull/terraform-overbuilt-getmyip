f5b4980d0e1da83ef794e4d28c4471b3b0bea2a6
Alex Trull <git-and-sig.alex@trull.org>
2023-11-18 00:50:50 +0100

    renamed outputs so they look better.

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 
-output "ipv4_matches" {
+output "ipv4_all_matches" {
   value = local.ipv4_matches
 }
 
-output "ipv6_matches" {
+output "ipv6_all_matches" {
   value = local.ipv6_matches
 }
 
-output "ipv4_most_common_response" {
+output "ipv4" {
   value = join("", local.ipv4_most_common_response)
 }
 
-output "ipv6_most_common_response" {
+output "ipv6" {
   value = join("", local.ipv6_most_common_response)
 }
