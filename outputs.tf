87ebed505235f98b934575cae3a147c313f9d497
Alex Trull <git-and-sig.alex@trull.org>
2023-11-19 11:09:32 +0100

    Reordered outputs

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,19 @@
-
-output "ipv4_all_matches" {
-  value       = local.ipv4_matches
-  description = "List of all the ipv4 matches (informational)"
-}
-
-output "ipv6_all_matches" {
-  value       = local.ipv6_matches
-  description = "List of all the ipv6 matches (informational)"
-}
-
 output "ipv4" {
   value       = join("", local.ipv4_most_common_response)
-  description = "The most common ipv4 myip response"
+  description = "The most common ipv4 response"
 }
 
 output "ipv6" {
   value       = join("", local.ipv6_most_common_response)
-  description = "The most common ipv6 myip response"
+  description = "The most common ipv6 response"
+}
+
+output "ipv4_all_matches" {
+  value       = local.ipv4_matches
+  description = "List of all the ipv4 matches (informational/testing)"
+}
+
+output "ipv6_all_matches" {
+  value       = local.ipv6_matches
+  description = "List of all the ipv6 matches (informational/testing)"
 }
